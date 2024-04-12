1413258621d9f5b60e8922f69b9bedaaf8bf46c6
Michael <m.langguth@fz-juelich.de>
2024-04-12 13:13:24 +0200

    bug: avoid hard-coding of path for normaliation datafile.

--- atmorep/datasets/normalizer_local.py
+++ atmorep/datasets/normalizer_local.py
@@ -14,6 +14,7 @@
 #
 ####################################################################################################
 
+import os
 import code
 import numpy as np
 import xarray as xr
@@ -24,7 +25,7 @@ class NormalizerLocal() :
 
   def __init__(self, field_info, vlevel, file_shape, data_type = 'era5', level_type = 'ml') :
 
-    fname_base = './data/{}/normalization/{}/normalization_mean_var_{}_y{}_m{:02d}_{}{}.bin'
+    fname_base =  '{}/{}/normalization/{}/normalization_mean_var_{}_y{}_m{:02d}_{}{}.bin'
     self.year_base = config.datasets[data_type]['extent'][0][0]
     self.year_last = config.datasets[data_type]['extent'][0][1]
     lat_min, lat_max = config.datasets[data_type]['extent'][1]
@@ -37,7 +38,7 @@ class NormalizerLocal() :
     self.corr_data = [ ]
     for year in range( self.year_base, self.year_last+1) :
       for month in range( 1, 12+1) :
-        corr_fname = fname_base.format( data_type, field_info[0], field_info[0],
+        corr_fname = fname_base.format(str(config.path_data), data_type, field_info[0], field_info[0],
                                         year, month, level_type, vlevel)
         ns_lat = int( (lat_max-lat_min) / res + 1)
         ns_lon = int( (lon_max-lon_min) / res + (0 if is_global else 1) )
